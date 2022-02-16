5d0526d087e2f4461d6c47a4e5b0d4541bf84e06
Alina Rao <88941807+AlinaTaoRao@users.noreply.github.com>
2022-02-16 19:07:01 +0100

    fix lint

--- client/src/handlers/input-word.js
+++ client/src/handlers/input-word.js
@@ -8,7 +8,6 @@ import { renderList } from '../components/render-list.js';
  * It is called each time the user clicks the "add word" button.
  *
  * @param {Event} event - The event triggered when the user clicks the button.
- * @returns {Element} ul - Component, ul, of the user interface.
  */
 export const inputWord = (event) => {
   /* -- entry point for adding or removing a word -- */
