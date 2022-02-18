306d9376f4a5f7a312407549c2d06cc39baca89f
Alina Rao <88941807+AlinaTaoRao@users.noreply.github.com>
2022-02-18 18:36:03 +0100

    lint

--- client/src/logic/sort-strings.spec.js
+++ client/src/logic/sort-strings.spec.js
@@ -143,7 +143,7 @@ describe('sorts the strings in an array in different ways', () => {
       expect(sortStrings([], 'z')).toEqual([]);
     });
     it('uppercase and lowercase', () => {
-      const expected = ['BCD', 'bcd', 'ABC', 'abc']
+      const expected = ['BCD', 'bcd', 'ABC', 'abc'];
       expect(sortStrings(['abc', 'bcd', 'ABC', 'BCD'], 'z')).toEqual(expected);
     });
     it('mixed', () => {
