b7cc3dba0f1ee5992e1776b3258649561d8613d3
Alina Rao <88941807+AlinaTaoRao@users.noreply.github.com>
2022-03-02 22:32:00 +0100

    fix lint error

--- client/src/logic/target-logic.js
+++ client/src/logic/target-logic.js
@@ -1,27 +1,31 @@
 /**
  * Target logic: which component the event targets at.
  *
- * @param {Element} t - The target of an event.
+ * @param {object} t - The target of an event.
+ * @returns {Boolean}.
  */
 export const targetAtNewPen = (t) => t.className === 'new-pen';
 
 /**
  * Target logic: which component the event targets at.
  *
- * @param {Element} t - The target of an event.
+ * @param {object} t - The target of an event.
+ * @returns {Boolean}.
  */
 export const targetAtRemPen = (t) => t.className === 'rem-pen';
 
 /**
  * Target logic: which component the event targets at.
  *
- * @param {Element} t - The target of an event.
+ * @param {object} t - The target of an event.
+ * @returns {Boolean}.
  */
 export const targetAtNewList = (t) => t.className === 'new-list-label';
 
 /**
  * Target logic: which component the event targets at.
  *
- * @param {Element} t - The target of an event.
+ * @param {object} t - The target of an event.
+ * @returns {Boolean}.
  */
 export const targetAtRemList = (t) => t.className === 'rem-list-label';
